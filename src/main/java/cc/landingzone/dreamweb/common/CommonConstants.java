9f7c80311c8ed238c44b8a0665eafeb079e92e49
charles.chengc <charles.chengc@alibaba-inc.com>
2021-01-22 17:44:08 +0800

    update weixin login

--- src/main/java/cc/landingzone/dreamweb/common/CommonConstants.java
+++ src/main/java/cc/landingzone/dreamweb/common/CommonConstants.java
@@ -16,6 +16,8 @@ public class CommonConstants {
 
     public static final String Aliyun_REGION_HANGZHOU = "cn-hangzhou";
 
+    public static final String WEB_LANDINGZONE_ID;
+    public static final String WEB_LANDINGZONE_SECRET;
 
     // 是否线上环境
     public static final boolean ENV_ONLINE;
@@ -25,6 +27,8 @@ public class CommonConstants {
         ENV_ONLINE = Boolean.parseBoolean(properties.getProperty("dreamweb.env_online"));
         Aliyun_AccessKeyId = properties.getProperty("dreamweb.aliyun_accesskeyid");
         Aliyun_AccessKeySecret = properties.getProperty("dreamweb.aliyun_accesskeysecret");
+        WEB_LANDINGZONE_ID = properties.getProperty("dreamweb.weixin.landingzone.id");
+        WEB_LANDINGZONE_SECRET = properties.getProperty("dreamweb.weixin.landingzone.secret");
     }
 
     public static Properties loadProperties() {
