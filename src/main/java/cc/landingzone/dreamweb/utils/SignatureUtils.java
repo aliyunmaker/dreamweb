1f39a0261e83df259fa2c07fe1dee1cbf5ec7bd0
merc-bottle <bingyang_cby@163.com>
2021-02-04 14:23:04 +0800

    feat: 编写说明文档

--- src/main/java/cc/landingzone/dreamweb/utils/SignatureUtils.java
+++ src/main/java/cc/landingzone/dreamweb/utils/SignatureUtils.java
@@ -3,7 +3,6 @@ package cc.landingzone.dreamweb.utils;
 import javax.crypto.Mac;
 import javax.crypto.spec.SecretKeySpec;
 
-import cc.landingzone.dreamweb.controller.LoginController;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.util.Assert;
@@ -14,24 +13,37 @@ import org.springframework.util.Assert;
  */
 public class SignatureUtils {
 
-    private static Logger logger = LoggerFactory.getLogger(LoginController.class);
+    private static Logger logger = LoggerFactory.getLogger(SignatureUtils.class);
 
     private static final String DEFAULT_CHARSET = "UTF-8";
     private static final long ONE_SECOND = 1000L;
     private static final long ONE_MINUTE = 60 * ONE_SECOND;
     private static final long THIRTY_MINUTES = 30 * ONE_MINUTE;
 
+    /**
+     * 校验签名
+     *
+     * @param params
+     * @param accessKeySecret
+     * @param signature
+     */
     public static void checkSignature(String params, String accessKeySecret, String signature) {
         Assert.isTrue(signature.equals(generateSignature(params, accessKeySecret)), "签名验证失败!");
     }
 
-    public static String generateSignature(String params, String accessKeySecret) {
-        String stringToSign = params;
+    /**
+     * 计算签名
+     *
+     * @param needSign 待签名字符串
+     * @param accessKeySecret 通过DreamWeb系统申请的AccessKey Secret
+     * @return
+     */
+    public static String generateSignature(String needSign, String accessKeySecret) {
         try {
             Mac mac = Mac.getInstance("HmacSHA1");
             SecretKeySpec signingKey = new SecretKeySpec(accessKeySecret.getBytes(DEFAULT_CHARSET), mac.getAlgorithm());
             mac.init(signingKey);
-            byte[] rawHmac = mac.doFinal(stringToSign.getBytes(DEFAULT_CHARSET));
+            byte[] rawHmac = mac.doFinal(needSign.getBytes(DEFAULT_CHARSET));
             return toHexString(rawHmac);
         } catch (Exception e) {
             logger.error(e.getMessage(), e);
@@ -57,6 +69,12 @@ public class SignatureUtils {
         return hexString.toString();
     }
 
+    /**
+     * 检查签名是否过期
+     *
+     * @param now
+     * @param timestamp
+     */
     public static void checkTimestamp(long now, String timestamp) {
         Assert.isTrue(Math.abs(now - Long.parseLong(timestamp)) <= THIRTY_MINUTES, "签名已经过期!");
     }
