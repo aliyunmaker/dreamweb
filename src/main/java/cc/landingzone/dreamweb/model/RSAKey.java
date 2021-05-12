85b4c4d192f08068fcd03816a3b48bd4f5182d77
yangjob <923064497@qq.com>
2021-05-12 16:41:40 +0800

    将密钥存放在数据库中,并进行查询更改,前端index.html添加"系统密钥管理"菜单

--- src/main/java/cc/landingzone/dreamweb/model/RSAKey.java
+++ src/main/java/cc/landingzone/dreamweb/model/RSAKey.java
@@ -1,10 +1,18 @@
 package cc.landingzone.dreamweb.model;
 
 public class RSAKey {
+    private Integer id;
     private String publicKey;
     private String privateKey;
 
     public RSAKey(String publicKey, String privateKey) {
+        this.id = 0;
+        this.publicKey = publicKey;
+        this.privateKey = privateKey;
+    }
+
+    public RSAKey(int id, String publicKey, String privateKey) {
+        this.id = id;
         this.publicKey = publicKey;
         this.privateKey = privateKey;
     }
@@ -15,5 +23,13 @@ public class RSAKey {
 
     public String getPrivateKey() {
         return privateKey;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
     }
 }
