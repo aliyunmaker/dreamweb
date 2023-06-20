b0ca7e9fb8514b85c27fd28667f54ff124106188
恬裕 <hupeixuan.hpx@alibaba-inc.com>
2023-06-20 10:46:07 +0800

    optimize listApps.do

--- src/main/java/cc/landingzone/dreamweb/demo/appcenter/ApplicationController.java
+++ src/main/java/cc/landingzone/dreamweb/demo/appcenter/ApplicationController.java
@@ -9,14 +9,16 @@ import cc.landingzone.dreamweb.demo.appcenter.model.Application;
 import cc.landingzone.dreamweb.demo.appcenter.model.Resource;
 import cc.landingzone.dreamweb.demo.appcenter.model.Service;
 import com.aliyun.tag20180828.models.ListResourcesByTagResponseBody;
-import org.apache.commons.lang3.StringUtils;
+import com.aliyun.tag20180828.models.ListTagResourcesResponseBody;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Controller
 @RequestMapping("/apps")
@@ -47,20 +49,12 @@ public class ApplicationController extends BaseController {
         WebResult result = new WebResult();
         try {
             String appName = request.getParameter("appName");
-            String serviceName = request.getParameter("serviceName");
 
             List<Resource> resources = new ArrayList<>();
 
-            // 如果serviceName为空，查询所有services
-            if (StringUtils.isBlank(serviceName)) {
-                for (ServiceEnum serviceEnum: ServiceEnum.values()) {
-                    Service service = getServiceByApplication(appName, serviceEnum);
-                    resources.addAll(getResourcesByService(service, appName));
-                }
-            } else {
-                ServiceEnum serviceEnum = ServiceEnum.valueOf(serviceName);
+            for (ServiceEnum serviceEnum: ServiceEnum.values()) {
                 Service service = getServiceByApplication(appName, serviceEnum);
-                resources = getResourcesByService(service, appName);
+                resources.addAll(getResourcesByService(service, appName));
             }
 
             result.setTotal(resources.size());
@@ -77,15 +71,39 @@ public class ApplicationController extends BaseController {
         Application application = new Application();
         application.setAppName(applicationEnum.name());
         application.setDescription(applicationEnum.getDescription());
-        List<Service> serviceList = new ArrayList<>();
-
-        for (ServiceEnum serviceEnum: ServiceEnum.values()) {
-            Service service = getServiceByApplication(applicationEnum.name(), serviceEnum);
-            assert service != null;
-            serviceList.add(service);
-        }
-        application.setServices(serviceList);
+        application.setServicesCount(listServicesByApplication(applicationEnum.name()));
         return application;
+    }
+
+    public Map<String, Integer> listServicesByApplication(String appName) {
+        Map<String, Integer> servicesCount = new HashMap<>(ServiceEnum.values().length);
+        for (ServiceEnum serviceEnum: ServiceEnum.values()) {
+            servicesCount.put(serviceEnum.name(), 0);
+        }
+
+        com.aliyun.tag20180828.Client client = createTagClient(CommonConstants.Aliyun_AccessKeyId, CommonConstants.Aliyun_AccessKeySecret);
+
+        try {
+            assert client != null;
+
+            com.aliyun.tag20180828.models.ListTagResourcesRequest listTagResourcesRequest = new com.aliyun.tag20180828.models.ListTagResourcesRequest()
+                    .setRegionId(CommonConstants.Aliyun_REGION_HANGZHOU)
+                    .setTags("{\"Application\":\""+ appName + "\"}")
+                    .setPageSize(1000);
+            com.aliyun.teautil.models.RuntimeOptions runtime = new com.aliyun.teautil.models.RuntimeOptions();
+            com.aliyun.tag20180828.models.ListTagResourcesResponse response = client.listTagResourcesWithOptions(listTagResourcesRequest, runtime);
+            List<ListTagResourcesResponseBody.ListTagResourcesResponseBodyTagResources> resourcesResponse = response.getBody().getTagResources();
+
+            for (ListTagResourcesResponseBody.ListTagResourcesResponseBodyTagResources resource: resourcesResponse) {
+                String resourceType = resource.getResourceARN().split(":")[2].toUpperCase();
+                servicesCount.merge(resourceType, 1, Integer::sum);
+            }
+
+            return servicesCount;
+        } catch (Exception e) {
+            logger.error(e.getMessage(), e);
+            return null;
+        }
     }
 
     public Service getServiceByApplication(String appName, ServiceEnum serviceEnum) {
