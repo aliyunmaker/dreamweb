24a196e3f56480ec89cb855e802c49b6f2c56ee6
珈 <liuyijia.lyj@alibaba-inc.com>
2023-07-08 15:59:42 +0800

    employee List

---
+++ src/main/java/cc/landingzone/dreamweb/demo/employeelist/model/ScimGroupResponse.java
@@ -0,0 +1,47 @@
+package cc.landingzone.dreamweb.demo.employeelist.model;
+
+import com.alibaba.fastjson.annotation.JSONField;
+
+import java.util.List;
+
+public class ScimGroupResponse {
+
+    @JSONField(name = "Resources")
+    private List<ScimGroup> resources;
+    private int itemsPerPage;
+    private int startIndex;
+    private int totalResults;
+
+    public List<ScimGroup> getResources() {
+        return resources;
+    }
+
+    public void setResources(List<ScimGroup> resources) {
+        this.resources = resources;
+    }
+
+    public int getItemsPerPage() {
+        return itemsPerPage;
+    }
+
+    public void setItemsPerPage(int itemsPerPage) {
+        this.itemsPerPage = itemsPerPage;
+    }
+
+    public int getStartIndex() {
+        return startIndex;
+    }
+
+    public void setStartIndex(int startIndex) {
+        this.startIndex = startIndex;
+    }
+
+    public int getTotalResults() {
+        return totalResults;
+    }
+
+    public void setTotalResults(int totalResults) {
+        this.totalResults = totalResults;
+    }
+
+}
