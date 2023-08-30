2ef3fecf4c76b31c2b74de8d740c831018870f00
程超 <charles.chengc@alibaba-inc.com>
2023-08-30 13:51:31 +0800

    set login name when go to index

--- src/main/java/cc/landingzone/dreamcmp/common/SystemController.java
+++ src/main/java/cc/landingzone/dreamcmp/common/SystemController.java
@@ -29,17 +29,25 @@ public class SystemController extends BaseController implements InitializingBean
 
     private Logger logger = LoggerFactory.getLogger(SystemController.class);
 
-
     @Override
-    public void afterPropertiesSet() throws Exception {
-    }
-
+    public void afterPropertiesSet() throws Exception {}
 
     public static HttpSession getSession() {
-        ServletRequestAttributes attr = (ServletRequestAttributes) RequestContextHolder.currentRequestAttributes();
+        ServletRequestAttributes attr = (ServletRequestAttributes)RequestContextHolder.currentRequestAttributes();
         return attr.getRequest().getSession(true); // true == allow create
     }
 
+    @RequestMapping("/getLoginUserName.do")
+    public void getLoginUserName(HttpServletRequest request, HttpServletResponse response) {
+        WebResult result = new WebResult();
+        try {
+            String username = SecurityContextHolder.getContext().getAuthentication().getName();
+            result.setData(username);
+        } catch (Exception e) {
+            logger.error(e.getMessage(), e);
+        }
+        outputToJSON(response, result);
+    }
 
     @RequestMapping("/getSession.do")
     public void getSession(HttpServletRequest request, HttpServletResponse response) {
@@ -53,7 +61,6 @@ public class SystemController extends BaseController implements InitializingBean
         outputToJSON(response, result);
     }
 
-
     /**
      * 开放权限
      *
@@ -64,7 +71,7 @@ public class SystemController extends BaseController implements InitializingBean
     public void getStartInfo(HttpServletRequest request, HttpServletResponse response) {
         String result = new String();
         try {
-            result = "hostname: " + InetAddress.getLocalHost().getHostName() + " \n " + new Date() ;
+            result = "hostname: " + InetAddress.getLocalHost().getHostName() + " \n " + new Date();
         } catch (Exception e) {
             logger.error(e.getMessage(), e);
             result = e.getMessage();
@@ -80,25 +87,29 @@ public class SystemController extends BaseController implements InitializingBean
             String logoDiv = null;
             if (CommonConstants.ENV_ONLINE) {
 
-                String jarFilePath = ClassUtils.getDefaultClassLoader().getResource("").getPath().replace("!/BOOT-INF/classes!/", "");
+                String jarFilePath =
+                    ClassUtils.getDefaultClassLoader().getResource("").getPath().replace("!/BOOT-INF/classes!/", "");
                 if (jarFilePath.startsWith("file")) {
                     jarFilePath = jarFilePath.substring(5);
                 }
                 logger.info("jarFilePath:" + jarFilePath);
                 JarFile jarFile = new JarFile(jarFilePath);
                 // JarEntry entry = jarFile.getJarEntry("META-INF/MANIFEST.MF");
-//                InputStream inputStream = this.getClass().getClassLoader().getResourceAsStream("META-INF/MANIFEST.MF");
+                // InputStream inputStream =
+                // this.getClass().getClassLoader().getResourceAsStream("META-INF/MANIFEST.MF");
                 Manifest manifest = new Manifest(jarFile.getManifest());
                 String version = manifest.getMainAttributes().getValue("Version");
                 jarFile.close();
                 if (StringUtils.isBlank(version)) {
                     version = "online version";
                 }
-                logoDiv = "<div align=\"center\"><i style=\"font-size:30px;margin-top:5px;color:#CFDEEF;animation-duration: 20s;\" class=\"fa fa-sun-o fa-spin\" aria-hidden=\"true\"></i></div><div align='center' style='background-color:#FF594C;margin-top:5px;font-size: 12px;'><font "
+                logoDiv =
+                    "<div align=\"center\"><i style=\"font-size:30px;margin-top:5px;color:#CFDEEF;animation-duration: 20s;\" class=\"fa fa-sun-o fa-spin\" aria-hidden=\"true\"></i></div><div align='center' style='background-color:#FF594C;margin-top:5px;font-size: 12px;'><font "
                         + "style='color: white;'>" + username + "<br>" + version + "</font></div>";
             } else {
                 String version = "test version";
-                logoDiv = "<div align=\"center\"><i style=\"font-size:30px;margin-top:5px;color:#CFDEEF;animation-duration: 1s;\" class=\"fa fa-sun-o fa-spin\" aria-hidden=\"true\"></i></div><div align='center' style='background-color:rgb(93,168,48);margin-top:5px;font-size: 12px;"
+                logoDiv =
+                    "<div align=\"center\"><i style=\"font-size:30px;margin-top:5px;color:#CFDEEF;animation-duration: 1s;\" class=\"fa fa-sun-o fa-spin\" aria-hidden=\"true\"></i></div><div align='center' style='background-color:rgb(93,168,48);margin-top:5px;font-size: 12px;"
                         + "'><font style='color: white;'>" + username + "<br>" + version + "</font></div>";
             }
             result.setData(logoDiv);
@@ -109,6 +120,5 @@ public class SystemController extends BaseController implements InitializingBean
         }
         outputToJSON(response, result);
     }
-
 
 }
