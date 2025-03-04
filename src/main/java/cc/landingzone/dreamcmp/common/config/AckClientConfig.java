c862b2eae68035bf90fa441ef0aed94e098bb3d1
老豆 <doujiale.doujiale@alibaba-inc.com>
2025-03-04 14:59:53 +0800

    feat: fix ackClient

--- src/main/java/cc/landingzone/dreamcmp/common/config/AckClientConfig.java
+++ src/main/java/cc/landingzone/dreamcmp/common/config/AckClientConfig.java
@@ -1,6 +1,5 @@
 package cc.landingzone.dreamcmp.common.config;
 
-import com.aliyun.credentials.models.CredentialModel;
 import com.aliyun.cs20151215.Client;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
@@ -19,12 +18,9 @@ public class AckClientConfig {
 
     @Bean
     Client ackClient() throws Exception {
-        CredentialModel credential = crossAccountCredentialClient.getCredential();
         com.aliyun.teaopenapi.models.Config config = new com.aliyun.teaopenapi.models.Config()
-            .setAccessKeyId(credential.getAccessKeyId())
-            .setAccessKeySecret(credential.getAccessKeySecret())
-            .setSecurityToken(credential.getSecurityToken());
-        config.endpoint = "cs.cn-hangzhou.aliyuncs.com";
+            .setCredential(crossAccountCredentialClient)
+            .setEndpoint("cs.cn-hangzhou.aliyuncs.com");
         return new Client(config);
     }
 }
